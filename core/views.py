88ad9f044d4743ed2b33b29959b1424b0dcce9af
eduardamirelly <eduardamirelly404@outlook.com>
2021-01-16 21:54:22 -0300

    Testando tratamento do arquivo .xls

--- core/views.py
+++ core/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
+import pandas as pd
 
 # Create your views here.
 
 def listAlunos(request):
-    return render(request, 'listAlunos.html')
+    table_alunos = pd.read_excel('core/dataframes/alunos-ifrn-caico.xls')
+    table_alunos = table_alunos.rename(columns={'Matrícula': 'Matricula', 'Código Curso': 'codeCurso', 'Descrição do Curso': 'descCurso', 'Email Acadêmico': 'emailAcad', 'Situação no Curso': 'statusCurso'})
+
+    list_header = table_alunos.columns
+
+    return render(request, 'listAlunos.html', {'list_header': list_header})
