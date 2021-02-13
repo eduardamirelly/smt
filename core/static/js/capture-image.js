4c2c40a14f97df4d30633b23f2920bc1593f66e5
eduardamirelly <eduardamirelly404@outlook.com>
2021-02-13 19:18:36 -0300

    Em produção - captura de imagem

--- core/static/js/capture-image.js
+++ core/static/js/capture-image.js
@@ -1,7 +1,5 @@
 var video = document.querySelector("#videoElement");
 
-console.log('OIIIIIIIIIIIIIII');
-
 if (navigator.mediaDevices.getUserMedia) {
   navigator.mediaDevices.getUserMedia({ video: true })
     .then(function (stream) {
